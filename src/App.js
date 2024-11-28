3f7a7f47cba9fefea2374df5485e39d1c027bcea
Heena <Heena.garg@thoughtworks.com>
2024-11-28 13:04:09 +0530

    created a login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  function handleClick() {
+    console.log("Button is clicked");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex items-center justify-center h-screen">
+        <div className="flex h-96 p-9 w-96 bg-customRed justify-center rounded-2xl flex-col items-center">
+          <h1 className="text-3xl text-softCream text-center mb-9">
+            Login Page
+          </h1>
+          <input
+            type="text"
+            className="h-8 w-26 p-3 mt-7 rounded-lg"
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            className="h-8 w-26 p-3 mt-7 rounded-lg"
+            placeholder="password"
+          />
+        <button
+            className="flex text-2xl bg-softCream flex w-32 m-7 rounded-lg justify-center font-semibold"
+            onClick={handleClick}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
